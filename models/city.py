d9bbf9a751a68527658decd451157b9686607073
moifore <moiforejallah@gmail.com>
2023-02-13 17:56:55 +0000

    City Module

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" City Module """
+
+from models.base_model import BaseModel
+
+""" Inherits from 'BaseModel'
+	Attributes:
+		state_id (str): Stores the <state.id>
+		name (str): Name of the city
+"""
+
+state_id = ""
+name = ""
