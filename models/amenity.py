a89de9a332536fdb3c815ea84081df87a85053ad
moifore <moiforejallah@gmail.com>
2023-02-13 17:56:37 +0000

    Amenity Module

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is the 'amenity' module """
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+	""" Inherits from BaseModel
+	Attributes:
+		name (str): Name of the amenity
+	"""
+
+name = ""
