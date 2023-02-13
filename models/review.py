fe8aa9119921727d03aa85e822cc0034928e2492
moifore <moiforejallah@gmail.com>
2023-02-13 17:57:46 +0000

    Review module

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Review Module """
+
+from models.base_model import BaseModel
+
+class Review(BaseModel)
+	""" Inherits from BaseModel
+	Attributes:
+		place_id (str): Stores the <place_id>
+		user_id (str): Stores the <user_id>
+		text (str): Review text
+	"""
+
+	place_id = user_id = text = ""
