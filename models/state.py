e125797cf48ef0da7e78f05bff6bd6a494040578
moifore <moiforejallah@gmail.com>
2023-02-13 17:57:28 +0000

    State module

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" State Module """
+
+from models.base_models import BaseModel
+
+class State(BaseModel):
+""" Inherits from 'BaseModel'
+	Attributes:
+		name (str): Name of the State
+	"""
+
+name = ""
