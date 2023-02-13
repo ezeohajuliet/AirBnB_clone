8646dcd885781926d5410bc6a849e9fed5faef77
moifore <moiforejallah@gmail.com>
2023-02-13 18:00:23 +0000

    Init file

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,6 @@
 """ All Instances of FileStorage """
 
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.state import State
-
 
 storage = FileStorage()
 storage.reload()
